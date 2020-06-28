2a9b3e315243d6ebef0b2f87c5bab704e36ad4d0
Salman Sabir <Salmansabir42@gmail.com>
2020-06-28 12:18:13 +0500

    Create Dictionary Mechanics_course_2_assessment_2.py

---
+++ Dictionary Mechanics_course_2_assessment_2.py
@@ -0,0 +1,3 @@
+# 1. At the halfway point during the Rio Olympics, the United States had 70 medals, Great Britain had 38 medals, China had 45 medals, Russia had 30 medals, 
+and Germany had 17 medals. Create a dictionary assigned to the variable medal_count with the country names as the keys and the number of medals the country 
+had as each key’s value.
