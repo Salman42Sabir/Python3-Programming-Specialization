51cd94c0902390ee3b999e4e3b336f2734b237e9
Salman Sabir <Salmansabir42@gmail.com>
2020-06-30 10:57:44 +0500

    Update Assessment_Functions.py

--- Assessment_Functions.py
+++ Assessment_Functions.py
@@ -35,8 +35,24 @@ accum(intList)
 # 5. Write a function, length, that takes in a list as the input. If the length of the list is greater than or equal to 5, return “Longer than 5”. 
 # If the length is less than 5, return “Less than 5”.
 
-
+intList = [1,2,3,5,15,62]
+def length (numList):
+    count = 0
+    for num in numList:
+        count = count + 1
+    if count >= 5:
+        return "Longer than 5"
+    else:
+        return "Less than 5"
+length(intList)
 
 # 6. You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided 
 # by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide 
 # function within the sum function. Do not worry about decimals.
+
+def divide(num):
+    return num / 2
+def sum(num):
+    return (num / 2) + 6
+sum(divide(10))
+
