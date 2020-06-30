eac232716f2fd55b03953b52afce0b2a1c7f311b
Salman Sabir <Salmansabir42@gmail.com>
2020-06-30 09:28:29 +0500

    Update Assessment_Functions.py

--- Assessment_Functions.py
+++ Assessment_Functions.py
@@ -21,3 +21,16 @@ def change(val):
     final = val + nice
     return final
 change(user_input)
+
+# 4. Write a function, accum, that takes a list of integers as input and returns the sum of those integers.
+
+
+
+# 5. Write a function, length, that takes in a list as the input. If the length of the list is greater than or equal to 5, return “Longer than 5”. 
+# If the length is less than 5, return “Less than 5”.
+
+
+
+# 6. You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided 
+# by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide 
+# function within the sum function. Do not worry about decimals.
